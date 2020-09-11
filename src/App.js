1c683c96c0f78812718b8780a67fbae9526d888a
unknown <ddidce@gmail.com>
2020-09-12 01:50:49 +0900

    리액트 부트스트랩 설치.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          하이리액트
-        </a>
-      </header>
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
