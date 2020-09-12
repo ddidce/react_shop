f7bebb5eb3120e9c80b5f406f3f73f19aa5688ed
unknown <ddidce@gmail.com>
2020-09-13 00:02:58 +0900

    메인페이지.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
-
+/* eslint-disable*/
+import React, { useState } from 'react';
+// or less ideally
+import { Navbar, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
+import ShoesInfo from './shoesInfo';
+import Data from './data';
 
 function App() {
+  let [shoes, setShoes] = useState(Data);
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg" className="">
+        <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+
+    <Jumbotron className="background">
+      <h1>20% season OFF</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling
+        extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
+
+    <div className="container">
+      <div className="row">
+
+      </div>
+    </div>
+
+
+
     </div>
   );
 }
+
+const Card = () => {
+  return (
+    <div className="col-md-4">
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      <h4>제목</h4>
+      <p>설명 & 가격</p>
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/shoesInfo.js
@@ -0,0 +1,15 @@
+/* eslint-disable  */
+import React, { useState } from 'react'
+import Data from './data';
+
+const shoesInfo = () => {
+    let [shoes, setShoes] = useState(Data);
+    
+    return (
+        <div className="row">
+            
+        </div>
+    )
+}
+
+export default shoesInfo;
